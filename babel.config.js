614e7fa54aa3605303b192039479832911c19020
Anish George <Anish_George-GGN@external.mckinsey.com>
2019-08-13 16:18:39 +0530

    Got env config done. Need to use pipeline to setup .env.production file for different environments

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-	presets: ['module:metro-react-native-babel-preset'],
-	plugins: [],
+	presets: ['module:metro-react-native-babel-preset', 'module:react-native-dotenv']
 };
--- index.ios.js
+++ index.ios.js
@@ -6,7 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './src/components/app/App.ios';
 import {name as appName} from './app.json';
 import {EnvConfig} from './src/env-config';
+import { API_HOST } from 'react-native-dotenv'
 
-EnvConfig.init("IOS", "/api"); // failed :(
+EnvConfig.init("IOS", API_HOST); // failed :(
 
 AppRegistry.registerComponent(appName, () => App);
