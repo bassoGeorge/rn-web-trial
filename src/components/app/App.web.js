87bfbcf3a76d67aa680dba2c839819161ac97d27
Anish George <Anish_George-GGN@external.mckinsey.com>
2019-08-22 09:38:03 +0530

    Create a basic tab navigation setup for web

--- src/components/app/App.web.js
+++ src/components/app/App.web.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Provider} from 'react-redux';
 import {store} from '../../store';
 import {AppWrapperContainer} from './app-wrapper';
 import {EnvConfig} from '../../env-config';
 import ScTrial from '../sc-trial/ScTrial.web';
 
-import {createSwitchNavigator, createNavigator, StackRouter, SceneView} from '@react-navigation/core';
-import {createBrowserApp} from '@react-navigation/web';
+import {createSwitchNavigator, createNavigator, StackRouter, TabRouter, SceneView} from '@react-navigation/core';
+import {createBrowserApp, Link} from '@react-navigation/web';
 import Home from '../home/Home.web';
+import About from '../about/about.web';
 
 export class App extends React.Component {
 	render() {
 		return (
 			<AppWrapperContainer render={({title, subTitle, onClick}) =>
 				(
-					<div>
-						<h1>{title}</h1>
-						<p>{subTitle}</p>
-						<NavigationContainer/>
-					</div>
+						<div>
+							<h1>{title}</h1>
+							<p>{subTitle}</p>
+							<NavigationContainer/>
+						</div>
 				)
 			}
 			/>
@@ -28,12 +29,17 @@ export class App extends React.Component {
 
 class MainLayout extends React.Component {
 	render() {
-		const { descriptors, navigation } = this.props;
-		const activeKey = navigation.state.routes[navigation.state.index].key;
-		const descriptor = descriptors[activeKey];
+		const {descriptors, navigation} = this.props;
+		const activeKey                 = navigation.state.routes[navigation.state.index].key;
+		const descriptor                = descriptors[activeKey];
 
 		return (
 			<div>
+				<header css={`background-color: #f8f8f8; color: black; width: 100%; padding: 20px; display: flex; justify-content: flex-start;`}>
+					<span>Trial react + react native</span>
+					<Link routeName="Home">Home</Link>
+					<Link routeName="About">About</Link>
+				</header>
 				<div>
 					<SceneView
 						component={descriptor.getComponent()}
@@ -45,24 +51,68 @@ class MainLayout extends React.Component {
 	}
 }
 
-const HomeRouter = StackRouter({
-	Home: {
-		screen: Home,
-		path: '',
-	},
-	Details: {
-		screen: ScTrial,
-		path: 'details'
+class ScreenLayout extends React.Component {
+	render() {
+		const {descriptors, navigation} = this.props;
+		const activeKey                 = navigation.state.routes[navigation.state.index].key;
+		const descriptor                = descriptors[activeKey];
+		return (
+			<SceneView
+				component={descriptor.getComponent()}
+				navigation={descriptor.navigation}
+			/>
+		);
 	}
-}, {
-	initialRouteName: 'Home'
-});
+}
 
-const MainNavigator = createNavigator(
-	MainLayout,
-	HomeRouter,
-	{}
-);
+const HomeRouter    = StackRouter({
+	      Home   : {
+		      screen: Home,
+		      path  : '',
+	      },
+	      Details: {
+		      screen: ScTrial,
+		      path  : 'details',
+	      },
+      }, {
+	      initialRouteName: 'Home',
+      }),
+      HomeNavigator = createNavigator(
+	      ScreenLayout,
+	      HomeRouter,
+	      {},
+      );
+
+const AboutRouter    = StackRouter({
+	      About: {
+		      screen: About,
+		      path  : '',
+	      },
+      }),
+      AboutNavigator = createNavigator(
+	      ScreenLayout,
+	      AboutRouter,
+	      {},
+      );
+
+
+const MainRouter    = TabRouter({
+	      Home : {
+		      screen: HomeNavigator,
+		      path  : '',
+	      },
+	      About: {
+		      screen: AboutNavigator,
+		      path  : 'about',
+	      },
+      }, {
+	      initialRouteName: 'Home',
+      }),
+      MainNavigator = createNavigator(
+	      MainLayout,
+	      MainRouter,
+	      {},
+      );
 
 const NavigationContainer = createBrowserApp(MainNavigator);
 
