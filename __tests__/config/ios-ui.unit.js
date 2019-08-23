a3ace38779d5b64777e305b68bd52d5db2df52e8
Anish George <Anish_George-GGN@external.mckinsey.com>
2019-08-23 17:31:19 +0530

    Add @testing-library/react-native and complete ios ui testing

--- __tests__/config/ios-ui.unit.js
+++ __tests__/config/ios-ui.unit.js
@@ -3,5 +3,5 @@ const common = require('./common')
 module.exports = {
 	...common,
 	testRegex: "src/.*\\.ios\\.spec\\.jsx?$",
-	preset   : "react-native"
+	preset   : "@testing-library/react-native"
 }
--- src/modules/demo/components/about/About.ios.spec.js
+++ src/modules/demo/components/about/About.ios.spec.js
@@ -1,3 +1,11 @@
+import React from 'react'
+import {render} from '@testing-library/react-native'
+import {AboutComponent} from './About.ios'
+
 it("just passes", () => {
-	// Need to write some tests
+	const {getByText} = render(<AboutComponent title="__title__"/>)
+
+	expect(getByText("Some more text just to try out")).toBeTruthy()
+	expect(getByText("__title__ | About Page")).toBeTruthy()
+	expect(getByText("Deep navigation check")).toBeTruthy()
 })
