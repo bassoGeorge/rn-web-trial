c257b564f334ad11602f1e444eab4b07f78d858c
Anish George <Anish_George-GGN@external.mckinsey.com>
2019-08-26 14:31:05 +0530

    Add integration test for iOS

--- src/modules/demo/components/about/About.ios.js
+++ src/modules/demo/components/about/About.ios.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {View, Text, Button} from 'react-native'
+import {Button, Text, View} from 'react-native'
 import {AboutContainerGen} from './about.container';
 
-export function AboutComponent({title, navigation}) {
+export function AboutComponent({title, subTitle, navigation}) {
 	return(
 		<View css={`padding: 30px;`}>
 			<Text css={`font-size: 20px; color: blue;`}>{title} | About Page</Text>
-			<Text>Some more text just to try out</Text>
+			<Text>Some more text just to try out | {subTitle}</Text>
 			<Button
 				title="Deep navigation check"
 				onPress={() => navigation.navigate("Details", {name: "Deep check", id: 100})}
